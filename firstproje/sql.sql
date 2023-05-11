99e630bcbdd06b5b12f98cc56730e25add2aa470
Kjrey2003 <kajanirey@gmail.com>
2023-05-11 10:22:47 -0400

    added comments

--- firstproje/sql.sql
+++ firstproje/sql.sql
@@ -1,8 +1,9 @@
+--creates the tables
 --CREATE TABLE STUDENTS( name TEXT, age INTEGER,
   --  gender TEXT,  gpa  INTEGER, allergies  TEXT);
 
     
-
+--adds values to the tables
  /* INSERT INTO STUDENTS (name, age, gender, gpa, allergies)
         VALUES('Kajani', 19, 'male', 3.0, 'yes');
 
@@ -11,7 +12,7 @@
 
     INSERT INTO STUDENTS (name, age, gender, gpa, allergies)
         VALUES('Kamoi', 15, 'male', 2.6, 'none');*/
-
+    --made a mistake so i had to delete a student from the table 
    -- DELETE FROM STUDENTS
      --   WHERE name = 'Kajani';
     
@@ -19,10 +20,11 @@
    -- ALTER TABLE STUDENTS
      --   DROP COLUMN gender;
 
+    --added a column so i can be more specific with the alllergies 
     -- ALTER TABLE STUDENTS
       --  ADD COLUMN allergic TEXT;
    
-
+--added the allergies 
     UPDATE STUDENTS
         SET allergic = 'N/A'
         WHERE rowid = 3;
@@ -35,5 +37,5 @@
         SET allergic = 'Artichokes'
         WHERE rowid = 5;  
 
-    
+    --allows me to see all the content on the table
     SELECT * FROM STUDENTS;
